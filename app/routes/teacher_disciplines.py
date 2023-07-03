5fb9858b2cec3ef5bb1ef68be372d38309d23565
talich12 <denis.tal@mail.ru>
2023-07-03 21:35:06 +1000

    add_teachers_disciplines_api

---
+++ app/routes/teacher_disciplines.py
@@ -0,0 +1,161 @@
+from app import app, db, spec
+from flask import jsonify, request
+from marshmallow import Schema, fields
+from app.models import Teacher_discipline, Teacher_disciplineSchema, PostTeacher_disciplineSchema, SuccessSchema, IDParameter
+
+
+@app.route('/teachers-disciplines', methods=['GET'])
+def get_teachers_disciplines():
+    """Teachers API.
+    ---
+    get:
+      description: Get all teachers
+      responses:
+        200:
+          description: Return all teachers
+          content:
+            application/json:
+              schema: 
+                type: array
+                items: Teacher_disciplineSchema
+      tags:
+        - Teacher
+    """
+    teacher_discipline_schema = Teacher_disciplineSchema(many = True)
+    req = Teacher_discipline.query.all()
+    output = teacher_discipline_schema.dump(req)
+    return jsonify(output)
+
+with app.test_request_context():
+    spec.path(view=get_teachers_disciplines)
+
+
+@app.route('/teachers-disciplines', methods=['POST'])
+def post_teachers_disciplines():
+    """Create a cute furry animal endpoint.
+    ---
+    post:
+      description: Create a teacher
+      requestBody:
+        description: Request data for teacher
+        required: true
+        content:
+          application/json:
+            schema: PostTeacher_disciplineSchema
+      security:
+        - ApiKeyAuth: []
+      responses:
+        200:
+          description: If pet is created
+          content:
+            application/json:
+              schema: SuccessSchema
+      tags:
+        - Teacher
+    """
+    data = request.get_json(silent=True)
+    teacher_id = data['teacher_id']
+    discipline_id = data['discipline_id']
+    
+    teacher_discipline = Teacher_discipline(teacher_id = teacher_id, discipline_id = discipline_id)
+    db.session.add(teacher_discipline)
+    db.session.commit()
+
+    return {"message": "Success"}
+
+with app.test_request_context():
+    spec.path(view=post_teachers_disciplines)
+
+@app.route('/teachers-disciplines/<id>', methods=['GET'])
+def get_cur_teachers_discipline(id):
+    """Teachers API.
+    ---
+    get:
+      description: Get teacher by id
+      parameters:
+      - in: path
+        schema: IDParameter
+      responses:
+        200:
+          description: Return teacher
+          content:
+            application/json:
+              schema: Teacher_disciplineSchema
+      tags:
+        - Teacher
+    """
+    teacher_discipline_schema = Teacher_disciplineSchema(many = False)
+
+    req = Teacher_discipline.query.filter_by(id = id).first()
+    output = teacher_discipline_schema.dump(req)
+    return jsonify(output)
+
+with app.test_request_context():
+    spec.path(view=get_cur_teachers_discipline)
+
+
+@app.route('/teachers-disciplines/<id>', methods=['POST'])
+def edit_cur_teachers_discipline(id):
+    """Create a cute furry animal endpoint.
+    ---
+    post:
+      description: Create a teacher
+      parameters:
+      - in: path
+        schema: IDParameter
+      requestBody:
+        description: Request data for teacher
+        required: true
+        content:
+          application/json:
+            schema: PostTeacher_disciplineSchema
+      security:
+        - ApiKeyAuth: []
+      responses:
+        200:
+          description: If pet is created
+          content:
+            application/json:
+              schema: SuccessSchema
+      tags:
+        - Teacher
+    """
+    data = request.get_json(silent=True)
+    teacher_id = data['teacher_id']
+    discipline_id = data['discipline_id']
+
+    teacher_discipline = Teacher_discipline.query.filter_by(id = id).first()
+    teacher_discipline.teacher_id = teacher_id
+    teacher_discipline.discipline_id = discipline_id
+    db.session.commit()
+
+    return {"message": "Success"}
+
+with app.test_request_context():
+    spec.path(view=edit_cur_teachers_discipline)
+
+@app.route('/teachers-disciplines/<id>', methods=['DELETE'])
+def delete_cur_teachers_discipline(id):
+    """Teachers API.
+    ---
+    delete:
+      description: Get teacher by id
+      parameters:
+      - in: path
+        schema: IDParameter
+      responses:
+        200:
+          description: Return teacher
+          content:
+            application/json:
+              schema: SuccessSchema
+      tags:
+        - Teacher
+    """
+    teacher_discipline = Teacher_discipline.query.filter_by(id = id).first()
+    db.session.delete(teacher_discipline)
+    db.session.commit()
+    return {"message": "Success"}
+
+with app.test_request_context():
+    spec.path(view=delete_cur_teachers_discipline)
